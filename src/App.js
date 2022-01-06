a776b7cd2bbf18d243e5d8f5a68e02cfbaad22f1
Cserne <angeli.cserne@gmail.com>
2022-01-06 11:18:40 +0100

    Changes added.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 //import ButtonList from './buttonList'
 import ButtonListWithMap from './buttonListWithMap'
+import Profile from './profile';
+import Profile2 from './profile2';
+import Profile3 from './profile3';
+import Profile4 from './profile4';
 
 //const buttonText = "Click me!";
 
@@ -11,6 +15,10 @@ function App() {
         {/*<ButtonList text={buttonText} text2="blaBla" />*/}
         <ButtonListWithMap />
       </header>
+      <Profile />
+      <Profile2 />
+      <Profile3 />
+      <Profile4 />
 
     </div>
   );
